85872a57dd4ff57e195231a115904a7349423b75
GuilhermeKimoto <70413601+GuilhermeKimoto@users.noreply.github.com>
2020-11-13 15:15:46 -0300

    Adicionar arquivos de projeto.

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Goon.Integracao.Plugins.FromJdEdwards.ToCrm
+{
+    public class Class1
+    {
+    }
+}
