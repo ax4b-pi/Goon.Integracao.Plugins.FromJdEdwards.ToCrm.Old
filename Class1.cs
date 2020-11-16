a9b09dcbd0b4de3732aebed09866820aaffd3832
GuilhermeKimoto <70413601+GuilhermeKimoto@users.noreply.github.com>
2020-11-16 17:53:51 -0300

    Refatoração ECG

--- Class1.cs
+++ Class1.cs
@@ -25,13 +25,13 @@ namespace Goon.Integracao.Plugins.FromJdEdwards.ToCrm
             IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
             ITracingService tracing = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
 
-               (context.Depth > 1)
-                return;
+            //(context.Depth > 1)
+            //    return;
 
             if (!context.MessageName.Equals("create", StringComparison.InvariantCultureIgnoreCase))
                 return;
 
-            if (!context.PrimaryEntityName.Equals("d365_contratodevendas", StringComparison.InvariantCultureIgnoreCase))
+            if (!context.PrimaryEntityName.Equals("goon_ecg", StringComparison.InvariantCultureIgnoreCase))
                 return;
 
             if (!context.InputParameters.Contains("Target"))
@@ -40,65 +40,53 @@ namespace Goon.Integracao.Plugins.FromJdEdwards.ToCrm
             if (!(context.InputParameters["Target"] is Entity))
                 return;
 
+            var goon_ecgmultiline = "";
+
+            var entity = context.InputParameters["Target"] as Entity;
+
+            goon_ecgmultiline = entity.GetAttributeValue<string>("goon_ecg");
+            IOrganizationService service =
+                   ((IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory))).CreateOrganizationService(null);
+
             try
             {
 
-                IOrganizationService systemUserService =
-                    ((IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory))).CreateOrganizationService(null);
-
-                Entity entity = context.InputParameters["Target"] as Entity;
-                var goon_nomedocampoquetemlinguica = "";
-
-                if (!entity.Contains("goon_ecgmultiline"))
+           
+                if (entity.Contains("goon_ecgmultiline"))
                 {
-                    goon_nomedocampoquetemlinguica = entity.GetAttributeValue<string>("goon_ecgmultiline")
 
-                        this.PersistenciaRobo(systemUserService, context, entity.GetAttributeValue<string>("goon_ecg"));
+                    this.PersistenciaRobo(service, context,("goon_ecgmultiline"));
                 }
             }
             catch (InvalidPluginExecutionException ex)
             {
-                this.RegistraLogErro(entity, ex, goon_ecgmultiline);
+                this.RegistraLogErro( ex, goon_ecgmultiline, service);
             }
             catch (Exception ex)
             {
-                this.RegistraLogErro(entity, ex, goon_ecgmultiline);
+                this.RegistraLogErro(ex, goon_ecgmultiline, service);
             }
         }
 
-        private void PersistenciaRobo(IOrganizationService systemUserService, IPluginExecutionContext context, bool tipoContrato)
+        private void PersistenciaRobo(IOrganizationService service, IPluginExecutionContext context, string tipoContrato)
         {
-            int retornoSeq = 0;
-            string partRet = "";
-            string retFormatado = "";
-
-            //var query = new QueryExpression("d365_contratodevendas");
-            //query.NoLock = true;
-            //query.ColumnSet = new ColumnSet("d365_codigocontratocrm", "createdon", "d365_aditivoversao");
-            //query.Criteria.AddCondition("d365_aditivoversao", ConditionOperator.Equal, 0);
-            //query.Criteria.AddCondition("d365_codigocontratocrm", ConditionOperator.BeginsWith, "SVD");
-            //query.TopCount = 1;
-            //query.AddOrder("createdon", OrderType.Descending);
-
-            //var entityCollection = systemUserService.RetrieveMultiple(query);
-
-            this.RegistraLogErro();
-
             var newConta = new Entity("account");
             newConta["cnpj"] = "99999999999999";
             newConta["picklistqq"] = new OptionSetValue(32322121);
             newConta["picklistqq"] = new EntityReference("contact", new Guid(""));
-            systemUserService.Create(newConta);
+            service.Create(newConta);
         }
 
-        private void RegistraLogErro(Entity entity, Exception ex, goon_ecgmultiline)
+        private void RegistraLogErro(Exception ex, string goon_ecgmultiline, IOrganizationService service)
         {
             var newConta = new Entity("goon_logErros");
-            newConta["message"] = ex.Message;
-            newConta["body"] = goon_ecgmultiline;
-            newConta["robo"] = nomeDoRobo;
-            newConta["status"] = status;
-            systemUserService.Create(newConta);
+            //criar campos na entidade ECG
+            newConta["message"] = goon_messageerror;
+
+            newConta["body"] = goon_ecgbody;
+            newConta["robo"] = goon_ecgroboname;       // nome da entidade  
+            newConta["status"] = goon_ecgstatus;      //true false
+            service.Create(newConta);
         }
     }
 	
